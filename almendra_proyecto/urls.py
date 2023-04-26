a4a30176d163983011dc1f9a103b534447ca0a27
Franco David <david_franco@outlook.com.ar>
2023-04-26 19:00:27 -0300

    2

--- almendra_proyecto/urls.py
+++ almendra_proyecto/urls.py
@@ -24,10 +24,8 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('almendra_app.urls')),
-] 
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-
-
 
 
 
