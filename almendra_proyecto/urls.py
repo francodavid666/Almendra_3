b7a9cc85cf3cc156d5759e1c33dae14b89a4b874
Franco David <david_franco@outlook.com.ar>
2023-04-26 17:30:58 -0300

    25

--- almendra_proyecto/urls.py
+++ almendra_proyecto/urls.py
@@ -24,13 +24,19 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include ('almendra_app.urls')),
-] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+] 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
+
+
+
+#+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
 #en teoria ya no sirve este linea codigo
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+#if settings.DEBUG:
+ #   urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 '''urlpatterns += [                    #serve
     re_path(r'^media/(?P<path>.*)$', serve, {
