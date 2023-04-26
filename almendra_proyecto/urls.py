2d8fc30efcadefb8280e9dd960ba2395c0ce6258
Franco David <david_franco@outlook.com.ar>
2023-04-26 19:24:22 -0300

    14

--- almendra_proyecto/urls.py
+++ almendra_proyecto/urls.py
@@ -49,10 +49,6 @@ urlpatterns += [                    #serve
 #+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-
 '''
 urlpatterns += [                    #serve
     re_path(r'^media/(?P<path>.*)$', serve, {
