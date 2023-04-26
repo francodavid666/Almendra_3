79ffe553ccfda9084c515c8bbdcb7cf4a7e43639
Franco David <david_franco@outlook.com.ar>
2023-04-26 17:06:07 -0300

    21

--- almendra_proyecto/urls.py
+++ almendra_proyecto/urls.py
@@ -29,8 +29,8 @@ urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
 #en teoria ya no sirve este linea codigo
-#if settings.DEBUG:
- #   urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 urlpatterns += [                    #serve
     re_path(r'^media/(?P<path>.*)$', serve, {
