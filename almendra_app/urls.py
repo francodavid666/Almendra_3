7380e072137ad7a39e96cad7d6f0560261151999
Franco David <david_franco@outlook.com.ar>
2023-04-26 12:59:45 -0300

    saque_los_upload

--- almendra_app/urls.py
+++ almendra_app/urls.py
@@ -77,11 +77,11 @@ urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
-urlpatterns += [                    #serve
+'''urlpatterns += [                    #serve
     re_path(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT,
     }),
-]
+]'''
 
 
 
