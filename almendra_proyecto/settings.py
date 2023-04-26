3c39497968e811a58f124126c93758a652e01b1b
Franco David <david_franco@outlook.com.ar>
2023-04-26 19:11:40 -0300

    8

--- almendra_proyecto/settings.py
+++ almendra_proyecto/settings.py
@@ -182,7 +182,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = './media/' 
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'almendra_app\static/almendra_app\media')
 
 
 
