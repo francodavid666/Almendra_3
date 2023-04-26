4463849638b446b1eaa142578bdc581f8649bcea
Franco David <david_franco@outlook.com.ar>
2023-04-26 13:15:35 -0300

    asd

--- almendra_proyecto/urls.py
+++ almendra_proyecto/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('', include ('almendra_app.urls')),
 ] 
 
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
 
 
 #+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
