80b1db6f32d09afa401afcc50ba8e1e9f8e94399
david-repo <david_franco@outlook.com.ar>
2023-08-05 20:12:18 -0300

    termine-login

---
+++ formularioLogin/js/scripts.js
@@ -0,0 +1,15 @@
+const bgBlanco = document.getElementById('bg-blanco')
+
+
+
+gsap.to('.bg-blanco',{
+    scale:0,
+    transition:0,
+    duration:.6,
+    width: 100,
+    height: 100,
+})
+
+
+
+
