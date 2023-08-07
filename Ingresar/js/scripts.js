ff993faa4918985c56c53733015bb898aaaab7e5
david-repo <david_franco@outlook.com.ar>
2023-08-07 20:41:50 -0300

    empezando el menu

--- Ingresar/js/scripts.js
+++ Ingresar/js/scripts.js
@@ -1,11 +1,9 @@
 const bgBlanco = document.getElementById('bg-blanco')
 
-
-
 gsap.to('.bg-blanco',{
     scale:0,
     transition:0,
-    duration:.8,
+    duration:.6,
     width: 100,
     height: 100,
     position:'absolute',
